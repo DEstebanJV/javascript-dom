14444feeb28c714541fb9e62be784ab8d4804a6a
DestebanJV <davidji@iadb.org>
2026-04-24 10:40:47 -0500

    Feat: first commit

---
+++ Practica/ManipulandoElDom/app.js
@@ -0,0 +1,12 @@
+/* Seleccionar elementos por id, class, tag y atributo */
+/* por ID */
+// document.getElementById('app-title'),
+// document.getElementById('app-title')
+/* por clase */
+// document.getElementsByClassName('menu-items')
+/* por tag */
+// document.getElementsByTagName('menu')
+/* por atributo */
+// document.querySelectorAll('[href="#"]')
+
+//document.querySelector /* Siempre trae el primer elemento */
